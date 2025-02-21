a09ba4c50d170c15e0d17719cfa644141c71a9f7
Ryan <ryan.emv@gmail.com>
2025-02-21 16:42:33 -0300

    correção no inputSelect

--- fontes/front-end/src/components/InputSelect/index.js
+++ fontes/front-end/src/components/InputSelect/index.js
@@ -1,8 +1,16 @@
 import * as C from "./styles";
 
-const Select = ({ options, value, onChange }) => {
+const Select = ({
+  options,
+  value,
+  onChange,
+  placeholder = "Selecione a opção",
+}) => {
   return (
     <C.Select value={value} onChange={onChange}>
+      <option value="" disabled>
+        {placeholder}
+      </option>
       {options.map((option, index) => (
         <option key={index} value={option.value}>
           {option.label}
