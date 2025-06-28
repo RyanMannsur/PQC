2cffce2a1e2bf7c1553b006b7282913d71da7b25
Bernardodeas <bernardodeas0@gmail.com>
2025-06-28 12:00:33 -0300

    gera até a parte de movimentação de produtos

--- fontes/backend/txtGenerator.py
+++ fontes/backend/txtGenerator.py
@@ -1,8 +1,19 @@
 from datetime import datetime
-from routes.produto_routes import get_produtos
+from routes.produto_routes import *
 from routes.produto_routes import obter_produto
 from app import app
 
+#funções privadas
+def split(str, num):
+    return [str[start:start+num] for start in range(0, len(str), num)]
+def inserirPonto(str):
+    stringSplit = split(str, 3)
+    if not stringSplit:  # Safety check for empty input
+        return str
+    strFinal = stringSplit[0]
+    for i in range(1, len(stringSplit)):
+        strFinal = f'{strFinal}.{stringSplit[i]}'
+    return strFinal
 # Passo 0 - Executando o arquivo
 # Abra um novo terminal, execute os seguintes comandos:
 # docker exec -it backend sh
@@ -50,5 +61,24 @@ with app.app_context():
                 vlrDensidade = 0.0
             file.write(f'{produto[1]}{int(round(float(perpureza), 0)):03d}') # nomproduto e perpureza
             file.write(f'{round(float(vlrDensidade), 2):05.2f}\n'.replace('.', ',')) # vlrdensidade
-            
-               
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            """SELECT p.perpureza, p.vlrDensidade, mte.qtdEstoque
+             FROM produto p, movtoestoque mte
+             WHERE p.codproduto = mte.codproduto"""
+        )
+        produtosEmMovimento = cursor.fetchall()
+        for produto in produtosEmMovimento:
+            file.write('MM') # Subseção movimento
+            file.write('PR[.........]') #assume que tudo é PC, TODO: adicionar codigo do produto
+            file.write(f'{int(round(float(produto[0]), 0)):03d}') # perpureza
+            file.write(f'{round(float(produto[1]), 2):05.2f}'.replace('.', ',')) # vlrdensidade
+            quantidade = produto[2]
+            if quantidade < 0: 
+                quantidade = quantidade * -1 #movimentação de saída
+            quantidadeInt = int(round(quantidade, 0))
+            quantidadeFloat = (float(quantidade) - quantidadeInt) * 1000
+            file.write(f'{inserirPonto(str(quantidadeInt).zfill(12))},{int(quantidadeFloat):3d}\n')
+
+
