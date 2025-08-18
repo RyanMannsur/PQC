6277fef3de85cd581294950dbea5e147651c3399
Leo1551 <macleonardo96@gmail.com>
2025-08-18 18:34:28 -0300

    Header e Sidebar responsivos e alterados para se encaixarem melhor à página

--- fontes/front-end/src/components/Header/index.js
+++ fontes/front-end/src/components/Header/index.js
@@ -1,77 +1,114 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import { AppBar, Toolbar, Typography, IconButton, useMediaQuery, useTheme } from "@mui/material";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../Sidebar";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = ({ children, withSidebar, showSignoutIcon = true, labName, usuario, onSignout }) => {
-const navigate = useNavigate();
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down("sm")); // <-- Adicione esta linha
+  const navigate = useNavigate();
+  const [sidebarOpen, setSidebarOpen] = React.useState(false);
 
-const handleSignout = () => {
-  if (onSignout) {
-    onSignout(); 
-  }
-  navigate("/");
-};
+  const handleMenuClick = () => {
+    setSidebarOpen((prev) => !prev); // alterna entre abrir e fechar
+  };
 
-return (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      height: "100vh"
-      // Removido overflow: hidden para permitir scroll global
-    }}
-  >
-    <AppBar position="fixed" style={{ width: "100%", zIndex: 1201 }}>
-      <Toolbar style={{ justifyContent: "space-between" }}>
-        <Typography variant="subtitle1">
-          CEFETMG - Sistema dos Produtos Químicos Controlados
-        </Typography>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          {labName && (
-            <Typography
-              variant="subtitle1"
-              style={{ marginRight: "16px" }}
-            >
-              {labName}
-            </Typography>
-          )}
-          {usuario?.nomUsuario && (
-            <Typography variant="subtitle1" style={{ marginRight: "16px" }}>
-              {usuario.nomUsuario}
-            </Typography>
-          )}
-          {showSignoutIcon && (
-            <IconButton color="inherit" onClick={handleSignout}>
-              <ExitToAppIcon />
-            </IconButton>
-          )}
-        </div>
-      </Toolbar>
-    </AppBar>
-    <div
-      style={{
+  const handleSidebarClose = () => {
+    setSidebarOpen(false);
+  };
+
+  const handleSignout = () => {
+    if (onSignout) {
+      onSignout(); 
+    }
+    navigate("/");
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <AppBar
+        position="static"
+        style={{
+          width: "100%",
+          zIndex: 1201,
+          backgroundColor: "#01386a", // cor de fundo personalizada
+        }}
+      >
+        <Toolbar style={{ justifyContent: "space-between" }}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            {isSmall && withSidebar && (
+              <IconButton color="inherit" onClick={handleMenuClick}>
+                <MenuIcon />
+              </IconButton>
+            )}
+            <img
+              src="/SPQC.png"
+              alt="SPQC"
+              style={{ height: 40, marginLeft: 4 }} // diminui o marginLeft
+            />
+          </div>
+          <div style={{ display: "flex", alignItems: "center", minWidth: 0 }}>
+            {labName && (
+              <Typography
+                variant="subtitle1"
+                style={{
+                  marginLeft: 10,
+                  maxWidth: "100%",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  color: "#fff",
+                }}
+                title={labName}
+              >
+                {labName}
+              </Typography>
+            )}
+            {usuario?.nomUsuario && (
+              <Typography variant="subtitle1" style={{ marginRight: "16px", color: "#fff" }}>
+                {usuario.nomUsuario}
+              </Typography>
+            )}
+            {showSignoutIcon && (
+              <IconButton color="inherit" onClick={handleSignout}>
+                <ExitToAppIcon />
+              </IconButton>
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+      {/* Renderiza a Sidebar logo abaixo do Header em telas pequenas */}
+      {isSmall && withSidebar && (
+        <Sidebar
+          open={sidebarOpen}
+          onClose={handleSidebarClose}
+          sx={{ marginTop: 64 }} // Adiciona margem superior igual à altura do AppBar (normalmente 64px)
+        />
+      )}
+      <div style={{
         display: "flex",
         flexDirection: "row",
         flex: 1,
-        marginTop: "64px",
         overflow: "hidden",
-      }}
-    >
-      {withSidebar && <Sidebar />}
-      <div
-        style={{
-          flex: 1,
-          overflow: "auto",
-          paddingLeft: withSidebar ? 240 : 0,
-        }}
-      >
-        {children}
+      }}>
+        {/* Renderiza a Sidebar na lateral apenas em telas grandes */}
+        {!isSmall && withSidebar && (
+          <Sidebar />
+        )}
+        <div
+          style={{
+            flex: 1,
+            overflow: "auto",
+            paddingLeft: withSidebar && !isSmall ? 240 : 0,
+          }}
+        >
+          {children}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Header;
--- fontes/front-end/src/components/Sidebar/index.js
+++ fontes/front-end/src/components/Sidebar/index.js
@@ -1,124 +1,152 @@
+import React from "react";
 import {
-Drawer,
-List,
-ListItem,
-ListItemText,
-ListItemIcon,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  IconButton,
+  useMediaQuery,
+  useTheme,
+  Box,
+  Divider,
 } from "@mui/material";
-import { Home, Settings, Inventory, SwapHoriz, AddBox, PostAdd, Assessment, People, TableChart, Business, Store } from "@mui/icons-material";
+import {
+  Home,
+  Settings,
+  Inventory,
+  SwapHoriz,
+  AddBox,
+  PostAdd,
+  Assessment,
+  People,
+  TableChart,
+  Business,
+  Store,
+  Close,
+} from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
-const Sidebar = () => {
-const handleClearLabAndNavigate = () => {
-  localStorage.removeItem("labId");
-  localStorage.removeItem("labName");
-};
+const Sidebar = ({ open, onClose, sx }) => {
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down("sm"));
 
+  const handleClearLabAndNavigate = () => {
+    localStorage.removeItem("labId");
+    localStorage.removeItem("labName");
+  };
 
-return (
-  <Drawer
-    variant="permanent"
-    sx={{
-      width: 240,
-      flexShrink: 0,
-      position: "fixed",
-      top: 0,
-      left: 0,
-      height: "100vh",
-      [`& .MuiDrawer-paper`]: {
-        width: 240,
-        boxSizing: "border-box",
-        height: "100%",
-        paddingTop: "64px",
-      },
-    }}
-  >
-    <List>
-      <ListItem component={Link} to="/home">
-        <ListItemIcon>
-          <Home />
-        </ListItemIcon>
-        <ListItemText primary="Início" />
-      </ListItem>
-
-      <ListItem
-        component={Link}
-        to="/selecionar-lab"
-        onClick={handleClearLabAndNavigate}
-      >
-        <ListItemIcon>
-          <Settings />
-        </ListItemIcon>
-        <ListItemText primary="Trocar Laboratório" />
-      </ListItem>
-
-      <ListItem component={Link} to="/inventario">
-        <ListItemIcon>
-          <Inventory />
-        </ListItemIcon>
-        <ListItemText primary="Inventário" />
-      </ListItem>
-
-      <ListItem component={Link} to="/cadastrar-produto">
-        <ListItemIcon>
-          <PostAdd />
-        </ListItemIcon>
-        <ListItemText primary="Adicionar Produtos" />
-      </ListItem>
-
-      <ListItem component={Link} to="/transferencias">
-        <ListItemIcon>
-          <SwapHoriz />
-        </ListItemIcon>
-        <ListItemText primary="Transferências" />
-      </ListItem>
-
-      <ListItem component={Link} to="/implantar/novos-produtos">
-        <ListItemIcon>
-          <AddBox />
-        </ListItemIcon>
-        <ListItemText primary="Implantação" />
-      </ListItem>
-
-
+  return (
+    <Drawer
+      variant={isSmall ? "temporary" : "permanent"}
+      open={isSmall ? open : true}
+      onClose={isSmall ? onClose : undefined}
+      ModalProps={{
+        keepMounted: true, // Melhor performance mobile
+      }}
+      sx={{
+        ...sx,
+        width: isSmall ? 200 : 240,
+        flexShrink: 0,
+        position: isSmall ? "absolute" : "fixed",
+        left: 0,
+        height: "100vh",
+        [`& .MuiDrawer-paper`]: {
+          width: isSmall ? 200 : 240,
+          boxSizing: "border-box",
+          height: "100%",
+          paddingTop: "60px",
+        },
+      }}
+    >
+      {isSmall && (
+        <Box
+          sx={{
+            position: "absolute",
+            top: 8,
+            right: 8,
+            zIndex: 1,
+          }}
+        >
+          <IconButton onClick={onClose}>
+            <Close />
+          </IconButton>
+        </Box>
+      )}
+      <List>
+        <ListItem component={Link} to="/home">
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <ListItemText primary="Início" />
+        </ListItem>
+        <ListItem
+          component={Link}
+          to="/selecionar-lab"
+          onClick={handleClearLabAndNavigate}
+        >
+          <ListItemIcon>
+            <Settings />
+          </ListItemIcon>
+          <ListItemText primary="Trocar Laboratório" />
+        </ListItem>
+        <ListItem component={Link} to="/inventario">
+          <ListItemIcon>
+            <Inventory />
+          </ListItemIcon>
+          <ListItemText primary="Inventário" />
+        </ListItem>
+        <ListItem component={Link} to="/cadastrar-produto">
+          <ListItemIcon>
+            <PostAdd />
+          </ListItemIcon>
+          <ListItemText primary="Adicionar Produtos" />
+        </ListItem>
+        <ListItem component={Link} to="/transferencias">
+          <ListItemIcon>
+            <SwapHoriz />
+          </ListItemIcon>
+          <ListItemText primary="Transferências" />
+        </ListItem>
+        <ListItem component={Link} to="/implantar/novos-produtos">
+          <ListItemIcon>
+            <AddBox />
+          </ListItemIcon>
+          <ListItemText primary="Implantação" />
+        </ListItem>
         <ListItem component={Link} to="/produto">
           <ListItemIcon>
             <TableChart />
           </ListItemIcon>
           <ListItemText primary="Produtos" />
         </ListItem>
-        
-      <ListItem component={Link} to="/campus">
-        <ListItemIcon>
-          <Business />
-        </ListItemIcon>
-        <ListItemText primary="Campus" />
-      </ListItem>
-
-      <ListItem component={Link} to="/unidadeorganizacional">
-        <ListItemIcon>
-          <Assessment />
-        </ListItemIcon>
-        <ListItemText primary="Unidades" />
-      </ListItem>
-
-      <ListItem component={Link} to="/localestocagem">
-        <ListItemIcon>
-          <Store />
-        </ListItemIcon>
-        <ListItemText primary="Locais de Estocagem" />
-      </ListItem>
-
-      <ListItem component={Link} to="/usuarios">
-        <ListItemIcon>
-          <People />
-        </ListItemIcon>
-        <ListItemText primary="Usuários" />
-      </ListItem>
-      
-    </List>
-  </Drawer>
-);
+        <ListItem component={Link} to="/campus">
+          <ListItemIcon>
+            <Business />
+          </ListItemIcon>
+          <ListItemText primary="Campus" />
+        </ListItem>
+        <ListItem component={Link} to="/unidadeorganizacional">
+          <ListItemIcon>
+            <Assessment />
+          </ListItemIcon>
+          <ListItemText primary="Unidades" />
+        </ListItem>
+        <ListItem component={Link} to="/localestocagem">
+          <ListItemIcon>
+            <Store />
+          </ListItemIcon>
+          <ListItemText primary="Locais de Estocagem" />
+        </ListItem>
+        <ListItem component={Link} to="/usuarios">
+          <ListItemIcon>
+            <People />
+          </ListItemIcon>
+          <ListItemText primary="Usuários" />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
 };
 
 export default Sidebar;
