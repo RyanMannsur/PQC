e5007f85a195f12ae15a6d88037142edbb7ed1a4
Ryan Eduardo Mansur Vasconcelos <ryan.emv@gmail.com>
2025-08-19 18:46:32 -0300

    logica da implantação

--- fontes/front-end/src/pages/Home/index.js
+++ fontes/front-end/src/pages/Home/index.js
@@ -1,8 +1,11 @@
 
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as C from "./styles";
 import { FiUsers, FiPackage, FiPlusSquare, FiArchive, FiRefreshCw, FiRepeat, FiMap, FiLayers, FiMapPin } from "react-icons/fi";
 import { useLocal } from "../../contexts/local";
+import { obterEstoqueLocalEstocagem } from "../../services/produto/service";
+import Modal from "../../components/Modal";
 
 const features = [
   {
@@ -52,25 +55,68 @@ const features = [
   },
 ];
 
+
 const Home = () => {
+  const { labId, labName } = useLocal();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
-  const { labName } = useLocal();
+
+  useEffect(() => {
+    const fetchLabDetails = async () => {
+      if (!labId) return;
+      const { codCampus, codUnidade, codPredio, codLaboratorio } = labId;
+      try {
+        const produtos = await obterEstoqueLocalEstocagem(
+          codCampus, codUnidade, codPredio, codLaboratorio
+        );
+        const produtosComQuantidade = produtos.filter(produto =>
+          Array.isArray(produto.item) && produto.item.some(i => i.qtdEstoque > 0)
+        );
+        if (produtosComQuantidade.length === 0) {
+          setIsModalOpen(true);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar informações do laboratório:", error);
+      }
+    };
+    fetchLabDetails();
+  }, [labId]);
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    navigate("/implantacao");
+  };
+
+  if (!labName && !isModalOpen) {
+    return <C.TitleHome>Carregando informações...</C.TitleHome>;
+  }
 
   return (
-    <C.Container>
-      <C.HeaderHome>
-        <C.TitleHome>Bem-vindo{labName ? ` ao ${labName}` : ""}!</C.TitleHome>
-        <C.SubtitleHome>Escolha uma funcionalidade:</C.SubtitleHome>
-      </C.HeaderHome>
-      <C.CardsGrid>
-        {features.map((feature) => (
-          <C.CardButton key={feature.label} onClick={() => navigate(feature.path)}>
-            <C.CardIcon>{feature.icon}</C.CardIcon>
-            <C.CardLabel>{feature.label}</C.CardLabel>
-          </C.CardButton>
-        ))}
-      </C.CardsGrid>
-    </C.Container>
+    <>
+      <Modal
+        title="Implantação Necessária"
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+      >
+        Laboratório vazio. Você será redirecionado(a) para a implantação do local selecionado.
+      </Modal>
+      {labName && (
+        <C.Container>
+          <C.HeaderHome>
+            <C.TitleHome>Bem-vindo ao {labName}!</C.TitleHome>
+            <C.SubtitleHome>Escolha uma funcionalidade:</C.SubtitleHome>
+          </C.HeaderHome>
+          <C.CardsGrid>
+            {features.map((feature) => (
+              <C.CardButton key={feature.label} onClick={() => navigate(feature.path)}>
+                <C.CardIcon>{feature.icon}</C.CardIcon>
+                <C.CardLabel>{feature.label}</C.CardLabel>
+              </C.CardButton>
+            ))}
+          </C.CardsGrid>
+        </C.Container>
+      )}
+    </>
   );
 };
 
